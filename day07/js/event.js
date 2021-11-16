5f88cd190a6b74c522104d1827efeebccb275ded
reina91kr <jjangyunji@naver.com>
2021-11-16 12:48:57 +0900

    day07

---
+++ day07/js/event.js
@@ -0,0 +1,9 @@
+function showText(){
+    document.getElementById("desc").style.display="block";
+    document.getElementById("open").style.display="none";
+}
+
+function hideText(){
+    document.getElementById("desc").style.display="none";
+    document.getElementById("open").style.display="block";
+}
